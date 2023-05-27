c941059a45ec1389df5ec36a5899123b1838e43f
siriyaparn <siriyasriboonma@gmail.com>
2023-05-27 18:05:00 +0700

    feat: Hello world in js

---
+++ hello-world.js
@@ -0,0 +1,10 @@
+console.log("hello world!");
+console.log("hello world!");
+console.log("hello world!");
+console.log("hello world!");
+console.log("hello world!");
+console.log("hello world!");
+console.log("hello world!");
+console.log("hello world!");
+console.log("hello world!");
+console.log("hello world!");
